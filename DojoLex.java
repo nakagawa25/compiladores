3a2ed6926be94ac9e1fce02325a3c5a137a2db77
Lucas Nakagawa <53760877+nakagawa25@users.noreply.github.com>
2019-09-01 14:50:06 -0300

    Add files via upload
    Codigo fonte do projeto.

---
+++ DojoLex.java
@@ -0,0 +1,52 @@
+package ftt.ec;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+/* DOJO - Analisador Léxico - Compiladores
+ * Limpar arquivo e criar tabela de lexemas
+ * 
+ * Aluno: 
+ * Lucas Polizeli e Jonatas 
+ * João Marcos e Carlos
+ * Letícia e Thales
+ * BARRETO E LEOZIN
+ * Murilo e Murilo
+ * 
+ * 
+ */
+
+public class DojoLex {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Criar tabela de lexemas
+		
+		//System.getProperty("user.dir");
+		
+		File file = new File("D:\\Documentos\\Termomecanica\\Engenharia de Computação - 6\\Compiladores\\FTT-DOJO-Compiladores-2019-1-master\\src\\ftt\\ec\\jquery-3.4.1.js");
+		  
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		  
+		String st;
+		String full = "";
+		  
+		  
+		while ((st = br.readLine()) != null) {
+			  
+			if(st.trim().startsWith("//")) continue;
+			
+			full += st.trim() + " ";
+			 
+		}
+		  
+		full = full.replaceAll("/\\*.+\\*/", "");
+
+		FileWriter arquivo = new FileWriter("D:\\Documentos\\saida.txt");
+		PrintWriter gravadorArquivo = new PrintWriter(arquivo);
+
+		gravadorArquivo.printf(full);
+		arquivo.close();
+	}
+}
